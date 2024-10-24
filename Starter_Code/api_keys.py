ef1cab8f8ddaae11098beb31946f718d7fc269a9
Alyssandra <alyssandracalhoun@gmail.com>
2024-10-23 19:47:10 -0500

    in progress

--- Starter_Code/api_keys.py
+++ Starter_Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "f8eaf201d8d647869710e94029d54de5"
 
 # Geoapify API Key
 geoapify_key = "f8eaf201d8d647869710e94029d54de5"
